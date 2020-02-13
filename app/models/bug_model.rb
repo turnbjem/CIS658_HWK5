0c4fe17cd0c31069675ed76158766938fd6239bf
turnbjem <turnbjem@mail.gvsu.edu>
2020-02-13 16:09:28 -0500

    final Iteration 1

--- app/models/bug_model.rb
+++ app/models/bug_model.rb
@@ -2,4 +2,7 @@ class BugModel < ApplicationRecord
   enum issue_type: [:issue, :enhancement, :feature]
   enum priority: [:low, :medium, :high]
   enum status: [:open, :closed, :monitor]
+
+  validates :title, presence: true, length: {minimum: 5, maximum: 15}
+  validates :description, presence: true, length: {minimum: 20, maximum: 600}
 end
